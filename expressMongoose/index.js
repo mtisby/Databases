62272099c78d94fc117bf06d13ca5ad150effdcf
mtisby <marieltisby1998@gmail.com>
2021-09-17 12:17:32 -0700

    express app

--- expressMongoose/index.js
+++ expressMongoose/index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import mongoose from "mongoose"
+
+mongoose.connect('mongodb://localhost:27017/shopApp')
+    .then(() => {
+        console.log("mongo connected")
+    })
+    .catch(err => {
+        console.log("mongo connection error")
+    })
 
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+
+app.get('/dog', (req, res) => {
+    res.send("woof")
+})
 
 app.listen(port, () => {
     console.log(`listening on ${port}`)
