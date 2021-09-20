bee578b7b4c4e3ced8143cd53efec4926d359323
mtisby <marieltisby1998@gmail.com>
2021-09-20 14:04:45 -0700

    structure

--- expressMongoose/index.js
+++ expressMongoose/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express"
 import mongoose from "mongoose"
 import { Product } from './models/product.js';
 import methodOverride from "method-override"
