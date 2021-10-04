79badce2c3cb4d9eeaff64597852f56620ca31f1
mtisby <marieltisby1998@gmail.com>
2021-10-04 11:48:44 -0700

    homework

--- expressMongoose/index.js
+++ expressMongoose/index.js
@@ -3,28 +3,72 @@ import mongoose from "mongoose"
 import { Product } from './models/product.js';
 import methodOverride from "method-override"
 
+const Product = require('./models/product');
+const Farm = require('./models/farm')
+const categories = ['fruit', 'vegetable', 'dairy'];
 
-mongoose.connect('mongodb://localhost:27017/farmStand')
+
+mongoose.connect('mongodb://localhost:27017/farmStandTake2', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("mongo connected")
+        console.log("MONGO CONNECTION OPEN!!!")
     })
     .catch(err => {
-        console.log("mongo connection error")
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
     })
 
 const app = express();
 const port = 3000;
-const categories = ['fruit', 'vegetable', 'dairy'];
+
 
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended: true}))
 
-// farm routes
+// FARM ROUTES
+
+app.get('/farms', async (req, res) => {
+    const farms = await Farm.find({});
+    res.render('farms/index', { farms })
+})
 app.get('/farms/new', (req, res) => {
     res.render('farms/new')
 })
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id).populate('products');
+    res.render('farms/show', { farm })
+})
 
+app.delete('/farms/:id', async (req, res) => {
+    const farm = await Farm.findByIdAndDelete(req.params.id);
+
+    res.redirect('/farms');
+})
+
+
+app.post('/farms', async (req, res) => {
+    const farm = new Farm(req.body);
+    await farm.save();
+    res.redirect('/farms')
+})
+
+app.get('/farms/:id/products/new', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories, farm })
+})
+
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`/farms/${id}`)
+})
 
 // use async and await here because 
 // we are requesting data from our database
